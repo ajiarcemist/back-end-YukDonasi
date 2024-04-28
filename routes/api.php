6b1f3d90a8766b3f040f8edfc3bd356afaad7563
Andri Nur Hidayatulloh <andribis13@gmail.com>
2024-04-28 15:18:52 +0700

    last commit

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('campaigns')->group(function() {
     Route::get('/', [CampaignController::class, 'index']);
+    Route::get('/{id}', [CampaignController::class, 'detail']);
 });
